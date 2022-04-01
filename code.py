63da6bf9f3b580b9d32ec6ccf1d3fe4a0488e478
Rob Dimsdale-Zucker <robert.dimsdale@gmail.com>
2022-04-01 14:48:41 -0400

    Send oscillate signal every few seconds.

--- code.py
+++ code.py
@@ -1,21 +1,24 @@
 import board
 import pulseio
 import adafruit_irremote
+import time
+import array
 
-IR_PIN = board.IR_RX  # Pin connected to IR receiver.
+from adafruit_circuitplayground.express import cpx
 
-LOW = 800
-HIGH = 1600
-FIRST = 2200
+LOW = 775
+HIGH = 1550
+FIRST = 2250
 
 # see: https://learn.adafruit.com/ir-sensor/circuitpython
 #      https://learn.adafruit.com/infrared-ir-receive-transmit-circuit-playground-express-circuit-python
-pulses = pulseio.PulseIn(IR_PIN, maxlen=200, idle_state=True)
-
-decoder = adafruit_irremote.GenericDecode()
-
+pulses = pulseio.PulseIn(board.IR_RX, maxlen=50, idle_state=True)
 pulses.clear()
 pulses.resume()
+
+pulseout = pulseio.PulseOut(board.IR_TX, frequency=38000, duty_cycle=2 ** 15)
+
+decoder = adafruit_irremote.GenericDecode()
 
 first_part = [0,0,1,0,0,0,0,0,1,0,0,0,0,0,0,0]
 
@@ -28,52 +31,53 @@ commands = {
     "timer_decrease": [1,0,1,0,0,0,0,0,1,0,1,0]
 }
 
-last_part_1 = [0,0,0,0]
-last_part_2 = [0,0,1,0]
-last_part_3 = [1,0,0,0]
+last_part = [
+  [0,0,0,0],
+  [0,0,1,0],
+  [1,0,0,0]
+]
+
+current_last_part_index = 0
 
 print('starting')
 
-def process_pulses(pulses):
-    if len(pulses) != 33:
-        return
+def create_pulse(command):
+    command_pulse = commands[command]
+    
+    global current_last_part_index
+    last_past_pulse = last_part[current_last_part_index]
+    
+    logical_pulse = first_part.copy()
+    logical_pulse.extend(command_pulse)
+    logical_pulse.extend(last_past_pulse)
+    
+    current_last_part_index = current_last_part_index + 1
+    current_last_part_index = current_last_part_index % 3
+    
+    #print('logical pulse: ', logical_pulse)
+    
+    mapped_pulse = [FIRST]
+    for i in logical_pulse:
+        if i == 0:
+            val = LOW
+        else:
+            val = HIGH
+        mapped_pulse.append(val)
+        
+    #print('mapped pulse: ', mapped_pulse)
+    
+    return mapped_pulse
 
-    else:
-        mapped = []
-        midpoint = (LOW+HIGH)/2
-        for i, pulse in enumerate(pulses):
-            if i == 0:
-                mapped.append(2)
-                continue
-
-            if pulse > midpoint:
-                mapped.append(1)
-            else:
-                mapped.append(0)
-
-        first_part_subsection = mapped[1:17]
-        command_subsection = mapped[17:29]
-        last_part_subsection = mapped[29:33]
-
-        if first_part_subsection != first_part:
-            print('first part does not match: ', first_part_subsection)
-
-        if last_part_subsection != last_part_1 and last_part_subsection != last_part_2 and last_part_subsection != last_part_3:
-            print('last part does not match: ',last_part_subsection)
-
-        found_command = ""
-        for name, command in commands.items():
-            if command == command_subsection:
-                found_command = name
-                print('matched: ', name)
-                break
-
-        if found_command == "":
-            print('did not match any command: ', command_subsection)
-
-# Loop waiting to receive pulses.
 while True:
-    # Wait for a pulse to be detected.
-    detected = decoder.read_pulses(pulses)
-    process_pulses(detected)
+    ###
+    mapped_pulse = create_pulse("oscillate")
+    
+    cpx.red_led = True
+   
+    print('sending pulse: ', mapped_pulse)
+    pulseout.send(array.array('H', mapped_pulse))
+    print('sending done')
+    cpx.red_led = False
+    
+    time.sleep(4)
 
